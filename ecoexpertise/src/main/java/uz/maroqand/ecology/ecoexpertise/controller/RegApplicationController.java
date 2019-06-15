29239f4a5a2bab8542533d7c1dcb3a46463b4290
saydullo <saydullot>
2019-06-16 01:45:28 +0500

    Merge branches 'Akmalbek' and 'master' of http://bitbucket.smst.uz/scm/eco/ecology into Akmalbek
    # Conflicts:
    #	core/src/main/java/uz/maroqand/ecology/core/entity/expertise/Applicant.java
    #	core/src/main/java/uz/maroqand/ecology/core/service/billing/PaymentService.java
    #	core/src/main/java/uz/maroqand/ecology/core/service/billing/impl/PaymentServiceImpl.java
    #	ecoexpertise/src/main/java/uz/maroqand/ecology/ecoexpertise/controller/RegApplicationController.java

--- ecoexpertise/src/main/java/uz/maroqand/ecology/ecoexpertise/controller/RegApplicationController.java
+++ ecoexpertise/src/main/java/uz/maroqand/ecology/ecoexpertise/controller/RegApplicationController.java
@@ -50,8 +50,7 @@ public class RegApplicationController {
     private final OfferService offerService;
     private final PaymentService paymentService;
     @Autowired
-    public RegApplicationController(UserService userService, SoatoService soatoService, OpfService opfService, RegApplicationService regApplicationService, ClientService clientService, ActivityService activityService, ObjectExpertiseService objectExpertiseService, OrganizationService organizationService, ProjectDeveloperService projectDeveloperService) {
-    public RegApplicationController(UserService userService, SoatoService soatoService, OpfService opfService, RegApplicationService regApplicationService, ClientService applicantService, ActivityService activityService, ObjectExpertiseService objectExpertiseService, OrganizationService organizationService, ProjectDeveloperService projectDeveloperService, OfferService offerService, PaymentService paymentService) {
+    public RegApplicationController(UserService userService, SoatoService soatoService, OpfService opfService, RegApplicationService regApplicationService, ClientService clientService, ActivityService activityService, ObjectExpertiseService objectExpertiseService, OrganizationService organizationService, ProjectDeveloperService projectDeveloperService, OfferService offerService, PaymentService paymentService) {
         this.userService = userService;
         this.soatoService = soatoService;
         this.opfService = opfService;
@@ -290,7 +289,6 @@ public class RegApplicationController {
         if(regApplication == null){
             return "redirect:" + Urls.RegApplicationList;
         }
-        Payment payment = paymentService.checkRegApplicationPaymentStatus(regApplication);
         /*if (payment.getStatus() != PaymentStatus.Success) {
             billingService.getWorkingInvoiceDtoByRegIndividualAndPayment(regApplication, payment);
             regApplication.setPaymentId(payment.getId());
@@ -303,7 +301,7 @@ public class RegApplicationController {
             return "redirect:" + RegUrls.RegApplicationWaiting + "?id=" + regApplication.getId();
         }*/
 
-        model.addAttribute("payment", payment);
+        model.addAttribute("payment", new Payment());
         model.addAttribute("regApplication", regApplication);
 //        model.addAttribute("bank_url", Urls.RegApplicationBank + "?id=" + id);
         model.addAttribute("upay_url", Urls.RegApplicationPayment + "?id=" + id);
