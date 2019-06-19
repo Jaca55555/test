a07fc8d182f06af39aab128e7ac5952d949d8134
saydullo <saydullot>
2019-06-19 13:41:50 +0500

    Status page add comment

--- core/src/main/java/uz/maroqand/ecology/core/repository/expertise/CommentRepository.java
+++ core/src/main/java/uz/maroqand/ecology/core/repository/expertise/CommentRepository.java
@@ -3,5 +3,9 @@ package uz.maroqand.ecology.core.repository.expertise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.maroqand.ecology.core.entity.expertise.Comment;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    List<Comment> findByRegApplicationIdAndDeletedFalse(Integer id);
 }
--- core/src/main/java/uz/maroqand/ecology/core/service/expertise/CommentService.java
+++ core/src/main/java/uz/maroqand/ecology/core/service/expertise/CommentService.java
@@ -2,7 +2,13 @@ package uz.maroqand.ecology.core.service.expertise;
 
 import uz.maroqand.ecology.core.entity.expertise.Comment;
 
+import java.util.List;
+
 public interface CommentService {
+
     Comment getById(Integer id);
+
     Comment createComment(Comment comment);
+
+    List<Comment> getListByRegApplicationId(Integer id);
 }
