f0c57adaa4bc20172ce85a283b16e9e4169e70f8
utkir <utkirbek.boltaev@gamail.com>
2019-07-09 16:13:12 +0500

    Coordinate, Conclusion

---
+++ core/src/main/java/uz/maroqand/ecology/core/repository/expertise/CoordinateRepository.java
@@ -0,0 +1,16 @@
+package uz.maroqand.ecology.core.repository.expertise;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.maroqand.ecology.core.entity.expertise.Coordinate;
+
+/**
+ * Created by Utkirbek Boltaev on 09.07.2019.
+ * (uz)
+ * (ru)
+ */
+@Repository
+public interface CoordinateRepository extends JpaRepository<Coordinate, Integer> {
+
+
+}
