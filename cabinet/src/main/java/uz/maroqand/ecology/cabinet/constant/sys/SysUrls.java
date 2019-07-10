2082dd5a79ce29ca48b94f7eeddb7847b9165385
bahrom0303 <bahrom.alimov.91@gmail.com>
2019-07-10 20:00:40 +0500

    appeal admin view,controller,urls

--- cabinet/src/main/java/uz/maroqand/ecology/cabinet/constant/sys/SysUrls.java
+++ cabinet/src/main/java/uz/maroqand/ecology/cabinet/constant/sys/SysUrls.java
@@ -15,4 +15,9 @@ public class SysUrls {
     public static final String NotificationList = Notification + "/list";
     public static final String NotificationView = Notification + "/view";
 
+    private static final String AppealAdmin = Prefix + "/appeal_admin";
+    public static final String AppealAdminList = AppealAdmin;
+    public static final String AppealAdminListAjax = AppealAdmin + "/list_ajax";
+    public static final String AppealAdminView = AppealAdmin + "/view";
+    public static final String AppealAdminSubCreate = AppealAdmin + "/sub_create";
 }
