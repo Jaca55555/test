500f6e08611fed589cd45a96cb9b631fab65a924
utkir <utkirbek.boltaev@gamail.com>
2019-08-21 12:05:32 +0500

    new service getEcoGovApi

--- ecoexpertise/src/main/java/uz/maroqand/ecology/ecoexpertise/controller/MainController.java
+++ ecoexpertise/src/main/java/uz/maroqand/ecology/ecoexpertise/controller/MainController.java
@@ -5,13 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import uz.maroqand.ecology.core.service.integration.common.EcoGovService;
 import uz.maroqand.ecology.ecoexpertise.constant.sys.SysTemplates;
 import uz.maroqand.ecology.ecoexpertise.constant.sys.SysUrls;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 /**
  * Created by Utkirbek Boltaev on 20.05.2019.
@@ -22,33 +20,16 @@ import java.net.URL;
 public class MainController {
 
     private final String apiKey = "800D00EAA115F8BB0D5083ECBDEC6ABDF9124235BDD6C2CD558CD686B583743524FB947AF54B11A6A98D3A303CF14CF6D86BD71C975E95B45E34D07F327674CA";
+    private final EcoGovService ecoGovService;
+
+    public MainController(EcoGovService ecoGovService) {
+        this.ecoGovService = ecoGovService;
+    }
 
     @RequestMapping("/")
-    public String getPage(Model model) throws Exception {
+    public String getPage(Model model) {
 
-
-        String url = "http://eco.gov.uz/uz/api/post/get";
-
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        // optional default is GET
-        con.setRequestMethod("POST");
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine="";
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        model.addAttribute("element",response.toString());
+        model.addAttribute("element", ecoGovService.getEcoGovApi());
         return "index";
     }
 
