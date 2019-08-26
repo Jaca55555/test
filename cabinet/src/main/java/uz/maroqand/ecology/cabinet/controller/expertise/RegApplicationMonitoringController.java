3f1e0dc0365994b4c03b3e82d1dfde7202ecb2ad
Akmal <saydullot>
2019-08-26 12:00:56 +0500

    reg application MNTR list and view

--- cabinet/src/main/java/uz/maroqand/ecology/cabinet/controller/expertise/RegApplicationMonitoringController.java
+++ cabinet/src/main/java/uz/maroqand/ecology/cabinet/controller/expertise/RegApplicationMonitoringController.java
@@ -4,16 +4,24 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uz.maroqand.ecology.cabinet.constant.expertise.ExpertiseTemplates;
 import uz.maroqand.ecology.cabinet.constant.expertise.ExpertiseUrls;
-import uz.maroqand.ecology.core.dto.expertise.FilterDto;
-import uz.maroqand.ecology.core.entity.expertise.RegApplication;
-import uz.maroqand.ecology.core.entity.expertise.RegApplicationInputType;
+import uz.maroqand.ecology.core.constant.expertise.CommentType;
+import uz.maroqand.ecology.core.constant.expertise.LogStatus;
+import uz.maroqand.ecology.core.dto.expertise.*;
+import uz.maroqand.ecology.core.entity.client.Client;
+import uz.maroqand.ecology.core.entity.expertise.*;
 import uz.maroqand.ecology.core.entity.user.User;
-import uz.maroqand.ecology.core.service.expertise.RegApplicationService;
+import uz.maroqand.ecology.core.repository.expertise.CoordinateLatLongRepository;
+import uz.maroqand.ecology.core.repository.expertise.CoordinateRepository;
+import uz.maroqand.ecology.core.service.billing.InvoiceService;
+import uz.maroqand.ecology.core.service.client.ClientService;
+import uz.maroqand.ecology.core.service.expertise.*;
 import uz.maroqand.ecology.core.service.sys.impl.HelperService;
 import uz.maroqand.ecology.core.service.user.UserService;
 import uz.maroqand.ecology.core.util.Common;
@@ -32,13 +40,29 @@ public class RegApplicationMonitoringController {
 
     private final UserService userService;
     private final RegApplicationService regApplicationService;
+    private final RegApplicationLogService regApplicationLogService;
     private final HelperService helperService;
+    private final ClientService clientService;
+    private final ChangeDeadlineDateService changeDeadlineDateService;
+    private final CoordinateRepository coordinateRepository;
+    private final CoordinateLatLongRepository coordinateLatLongRepository;
+    private final CommentService commentService;
+    private final InvoiceService invoiceService;
+    private final ProjectDeveloperService projectDeveloperService;
 
 
-    public RegApplicationMonitoringController(UserService userService, RegApplicationService regApplicationService, HelperService helperService) {
+    public RegApplicationMonitoringController(UserService userService, RegApplicationService regApplicationService, RegApplicationLogService regApplicationLogService, HelperService helperService, ClientService clientService, ChangeDeadlineDateService changeDeadlineDateService, CoordinateRepository coordinateRepository, CoordinateLatLongRepository coordinateLatLongRepository, CommentService commentService, InvoiceService invoiceService, ProjectDeveloperService projectDeveloperService) {
         this.userService = userService;
         this.regApplicationService = regApplicationService;
+        this.regApplicationLogService = regApplicationLogService;
         this.helperService = helperService;
+        this.clientService = clientService;
+        this.changeDeadlineDateService = changeDeadlineDateService;
+        this.coordinateRepository = coordinateRepository;
+        this.coordinateLatLongRepository = coordinateLatLongRepository;
+        this.commentService = commentService;
+        this.invoiceService = invoiceService;
+        this.projectDeveloperService = projectDeveloperService;
     }
 
     @RequestMapping(value = ExpertiseUrls.ExpertiseRegApplicationMonitoringList)
@@ -77,5 +101,44 @@ public class RegApplicationMonitoringController {
         }
         result.put("data",convenientForJSONArray);
         return result;
+    }
+
+    @RequestMapping(value = ExpertiseUrls.ExpertiseRegApplicationMonitoringView)
+    public String viewPage(
+            @RequestParam(name = "id") Integer id,
+            Model model
+    ){
+        RegApplication regApplication = regApplicationService.getById(id);
+        if (regApplication==null){
+            return "redirect:" + ExpertiseUrls.ExpertiseRegApplicationMonitoringList;
+        }
+        Client applicant = clientService.getById(regApplication.getApplicantId());
+        switch (applicant.getType()){
+            case Individual:
+                model.addAttribute("individual", new IndividualDto(applicant)); break;
+            case LegalEntity:
+                model.addAttribute("legalEntity", new LegalEntityDto(applicant)) ;break;
+            case ForeignIndividual:
+                model.addAttribute("foreignIndividual", new ForeignIndividualDto(applicant)); break;
+            case IndividualEnterprise:
+                model.addAttribute("individualEntrepreneur", new IndividualEntrepreneurDto(applicant)); break;
+        }
+
+        Coordinate coordinate = coordinateRepository.findByRegApplicationIdAndDeletedFalse(regApplication.getId());
+        if(coordinate != null){
+            List<CoordinateLatLong> coordinateLatLongList = coordinateLatLongRepository.getByCoordinateIdAndDeletedFalse(coordinate.getId());
+            model.addAttribute("coordinate", coordinate);
+            model.addAttribute("coordinateLatLongList", coordinateLatLongList);
+        }
+
+        model.addAttribute("invoice", invoiceService.getInvoice(regApplication.getInvoiceId()));
+        model.addAttribute("applicant", applicant);
+        model.addAttribute("projectDeveloper", projectDeveloperService.getById(regApplication.getDeveloperId()));
+
+        List<RegApplicationLog> regApplicationLogList = regApplicationLogService.getByRegApplicationId(regApplication.getId());
+        model.addAttribute("regApplication",regApplication);
+        model.addAttribute("regApplicationLogList",regApplicationLogList);
+
+        return ExpertiseTemplates.ExpertiseRegApplicationMonitoringView;
     }
 }
