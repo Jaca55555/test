2f858aaeeaaeafbc85065abb0a8431f89260872b
jamwid07 <jamwid07@mail.ru>
2020-01-06 16:12:00 +0500

    add create and view templates

--- docmanagment/src/main/java/uz/maroqand/ecology/docmanagment/constant/DocUrls.java
+++ docmanagment/src/main/java/uz/maroqand/ecology/docmanagment/constant/DocUrls.java
@@ -14,6 +14,7 @@ public class DocUrls {
     public static final String IncomeMailList = IncomeMail + "/list";
     public static final String IncomeMailListAjax = IncomeMail + "/list_ajax";
     public static final String IncomeMailView = IncomeMail + "/view";
+    public static final String IncomeMailNew = IncomeMail + "/new";
     public static final String IncomeMailEdit = IncomeMail + "/edit";
     public static final String IncomeMailExecuteEdit = IncomeMail + "/execute_edit";
 
