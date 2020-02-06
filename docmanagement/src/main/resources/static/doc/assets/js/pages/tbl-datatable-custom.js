cbc474e3269eebbbba11101c8cfb10061a7c9c6b
Sergey <stereshchuk98@gmail.com>
2020-02-06 18:02:55 +0500

    tables fix

--- docmanagement/src/main/resources/static/doc/assets/js/pages/tbl-datatable-custom.js
+++ docmanagement/src/main/resources/static/doc/assets/js/pages/tbl-datatable-custom.js
@@ -27,16 +27,6 @@ $(document).ready(function() {
         paging: false,
         fixedColumns: true,
     });
-    $('#fixed-columns-left-right').DataTable({
-        scrollY: "300px",
-        scrollX: true,
-        scrollCollapse: true,
-        paging: false,
-        fixedColumns: {
-            leftColumns: 1,
-            rightColumns: 1
-        }
-    });
     $('#fixed-header').DataTable({
         fixedHeader: true
     });
@@ -48,4 +38,23 @@ $(document).ready(function() {
         keys: true
     });
 
+    // [ Responsive-table ] start
+    $('#responsive-table').DataTable({
+    });
+
+    $('#responsive-table-model').DataTable({
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                    header: function(row) {
+                        var data = row.data();
+                        return 'Details for ' + data[0] + ' ' + data[1];
+                    }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+                    tableClass: 'table'
+                })
+            }
+        }
+    });
 });
