e5da9edff86f90e3ddfc47e75d8f553ae0e74d58
Akmalbek <sadullayev.tatuuf@gmail.com>
2020-02-24 20:44:00 +0500

    fixeds

--- docmanagement/src/main/java/uz/maroqand/ecology/docmanagement/controller/DocController.java
+++ docmanagement/src/main/java/uz/maroqand/ecology/docmanagement/controller/DocController.java
@@ -2,6 +2,7 @@ package uz.maroqand.ecology.docmanagement.controller;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uz.maroqand.ecology.core.entity.sys.File;
+import uz.maroqand.ecology.core.entity.user.User;
 import uz.maroqand.ecology.core.service.sys.FileService;
+import uz.maroqand.ecology.core.service.sys.impl.HelperService;
 import uz.maroqand.ecology.core.service.user.UserService;
 import uz.maroqand.ecology.core.util.Common;
 import uz.maroqand.ecology.docmanagement.constant.ControlForm;
@@ -45,6 +48,7 @@ public class DocController {
     private final DocumentLogService documentLogService;
     private final DocumentViewService documentViewService;
     private final DocumentOrganizationService documentOrganizationService;
+    private final HelperService helperService;
 
     @Autowired
     public DocController(
@@ -53,14 +57,15 @@ public class DocController {
             DocumentService documentService,
             DocumentLogService documentLogService,
             DocumentViewService documentViewService,
-            DocumentOrganizationService documentOrganizationService
-    ) {
+            DocumentOrganizationService documentOrganizationService,
+            HelperService helperService) {
         this.userService = userService;
         this.fileService = fileService;
         this.documentService = documentService;
         this.documentLogService = documentLogService;
         this.documentViewService = documentViewService;
         this.documentOrganizationService = documentOrganizationService;
+        this.helperService = helperService;
     }
 
     @RequestMapping(DocUrls.Dashboard)
@@ -131,7 +136,9 @@ public class DocController {
             @RequestParam(name = "file_ids", required = false) List<Integer> file_ids,
             DocumentLog documentLog
     ) {
-        documentLog.setCreatedById(userService.getCurrentUserFromContext().getId());
+        String locale = LocaleContextHolder.getLocale().getLanguage();
+        User user = userService.getCurrentUserFromContext();
+        documentLog.setCreatedById(user.getId());
         documentLog.setType(1);
         if (file_ids != null) {
             Set<File> files = new LinkedHashSet<>();
@@ -144,6 +151,9 @@ public class DocController {
         documentLog.setCreatedBy(userService.findById(documentLog.getCreatedById()));
         HashMap<String,Object> resutl = new HashMap<>();
         resutl.put("status", "success");
+        resutl.put("createName", user.getFullName());
+        resutl.put("createPosition", user.getPositionId()!=null?helperService.getUserPositionName(user.getPositionId(),locale):"");
+        resutl.put("createdAt", Common.uzbekistanDateAndTimeFormat.format(documentLog.getCreatedAt()));
         resutl.put("log", documentLog);
         return resutl;
     }
