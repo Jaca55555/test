c1b4e624b8b5d978c3968635c3770fe814934289
jamwid07 <jamwid07@mail.ru>
2020-03-11 19:37:25 +0500

    ODM-191 and ODM-179

--- docmanagement/src/main/java/uz/maroqand/ecology/docmanagement/controller/IncomingController.java
+++ docmanagement/src/main/java/uz/maroqand/ecology/docmanagement/controller/IncomingController.java
@@ -269,7 +269,7 @@ public class IncomingController {
         model.addAttribute("documentTaskSubs", documentTaskSubs);
         model.addAttribute("user", userService.getCurrentUserFromContext());
         model.addAttribute("comment_url", DocUrls.AddComment);
-        model.addAttribute("logs", documentLogService.getAllByDocAndTaskSubId(document.getId(), documentTaskSub.getId()));
+        model.addAttribute("logs", documentLogService.getAllByDocId(document.getId()));
         model.addAttribute("task_change_url", DocUrls.DocumentTaskChange);
         model.addAttribute("task_statuses", statuses);
         model.addAttribute("docList", documentService.findAllByDocumentTypeIn(docTypes, PageRequest.of(0,100, Sort.Direction.DESC, "id")));
