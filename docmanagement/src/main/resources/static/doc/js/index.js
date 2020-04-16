b222a64caab4865866b666793ebd28ce1b5657c3
sadullayevakmal <sadullayev.tatuuf@mail.ru>
2020-04-16 10:08:34 +0500

    print document

--- docmanagement/src/main/resources/static/doc/js/index.js
+++ docmanagement/src/main/resources/static/doc/js/index.js
@@ -119,3 +119,37 @@ function docFileName(files) {
         })
     }
 }
+
+function printThisDiv(id) {
+    try {
+        $("#"+id).printThis({
+            debug: true,               // show the iframe for debugging
+            importCSS: true,            // import parent page css
+            importStyle: true,         // import style tags
+            printContainer: false,       // print outer container/$.selector
+            loadCSS: [
+                "/static/doc/assets/plugins/bootstrap/css/bootstrap.css",
+                "/static/doc/css/my_style.css",
+                "/static/doc/assets/css/style.css"
+                    ],            // path to additional css file - use an array [] for multiple
+            pageTitle: "",              // add title to print page
+            removeInline: false,        // remove inline styles from print elements
+            removeInlineSelector: "*",  // custom selectors to filter inline styles. removeInline must be true
+            printDelay: 333,            // variable print delay
+            header: null,               // prefix to html
+            footer: null,               // postfix to html
+            base: false,                // preserve the BASE tag or accept a string for the URL
+            formValues: true,           // preserve input/form values
+            canvas: true,              // copy canvas content
+            doctypeString: '',       // enter a different doctype for older markup
+            removeScripts: true,       // remove script tags from print content
+            copyTagClasses: false,      // copy classes from the html & body tag
+            beforePrintEvent: null,     // function for printEvent in iframe
+            beforePrint: null,          // function called before iframe is filled
+            afterPrint: null            // function called before iframe is removed
+        });
+    }catch (e) {
+
+    }
+
+}
