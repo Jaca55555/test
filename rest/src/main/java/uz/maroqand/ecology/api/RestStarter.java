a3cabbccb7de3d69bb606d25377bff882d95c224
Akmalbek <sadullayev.tatuuf@gmail.com>
2020-09-25 13:45:04 +0500

    rest remove telegram

--- rest/src/main/java/uz/maroqand/ecology/api/RestStarter.java
+++ rest/src/main/java/uz/maroqand/ecology/api/RestStarter.java
@@ -10,11 +10,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import uz.maroqand.ecology.docmanagement.entity.DocumentTaskSub;
-import uz.maroqand.ecology.docmanagement.service.Bot;
+//import org.telegram.telegrambots.ApiContextInitializer;
+//import org.telegram.telegrambots.meta.TelegramBotsApi;
+//import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+//import uz.maroqand.ecology.docmanagement.entity.DocumentTaskSub;
+//import uz.maroqand.ecology.docmanagement.service.Bot;
 import uz.maroqand.ecology.core.service.user.UserService;
 import uz.maroqand.ecology.docmanagement.service.interfaces.DocumentTaskSubService;
 
@@ -26,25 +26,25 @@ import uz.maroqand.ecology.docmanagement.service.interfaces.DocumentTaskSubServi
 @Configuration
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @ComponentScan("uz.maroqand.ecology")
-@EnableJpaRepositories(basePackages = {"uz.maroqand.ecology.core.repository","uz.maroqand.ecology.docmanagement.repository"}, repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
-@EntityScan(basePackages = {"uz.maroqand.ecology.core.entity","uz.maroqand.ecology.docmanagement.entity"})
+@EnableJpaRepositories(basePackages = {"uz.maroqand.ecology.core.repository"}, repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
+@EntityScan(basePackages = {"uz.maroqand.ecology.core.entity"})
 @EnableCaching(proxyTargetClass = true)
 public class RestStarter {
 
 
     public static void main(String[] args) throws Throwable {
-        ApiContextInitializer.init();
+//        ApiContextInitializer.init();
 
         ConfigurableApplicationContext applicationContext = SpringApplication.run(RestStarter.class, args);
         RestStarter applicationStarter = applicationContext.getBean(RestStarter.class);
-        UserService userService =applicationContext.getBean(UserService.class);
-        DocumentTaskSubService documentTaskSubService = applicationContext.getBean(DocumentTaskSubService.class);
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+       /* UserService userService =applicationContext.getBean(UserService.class);
+        DocumentTaskSubService documentTaskSubService = applicationContext.getBean(DocumentTaskSubService.class);*/
+        /*TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
             telegramBotsApi.registerBot(new Bot(userService,documentTaskSubService));
         }catch (TelegramApiRequestException e){
             e.printStackTrace();
-        }
+        }*/
         System.out.println("======================== INITIALIZATION COMPLETED ========================");
 
     }
