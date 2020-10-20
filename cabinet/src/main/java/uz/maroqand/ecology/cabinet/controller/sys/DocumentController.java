a10798de8bd1ace916883f6bec9cbface2b36a2e
sadullayevakmal <sadullayev.tatuuf@mail.ru>
2020-10-20 07:56:40 +0500

    fixed all conclusion qr view

--- cabinet/src/main/java/uz/maroqand/ecology/cabinet/controller/sys/DocumentController.java
+++ cabinet/src/main/java/uz/maroqand/ecology/cabinet/controller/sys/DocumentController.java
@@ -123,7 +123,7 @@ public class DocumentController {
         int white = 255 << 16 | 255 << 8 | 255;
         int black = 0;
 
-        String url = "http://localhost:8085/" + SysUrls.GetDocument;
+        String url = "http://cb.eco-service.uz/repository/get-document";
 
         try {
             BitMatrix bitMatrix = writer.encode(url+"/"+documentRepo.getUuid(), BarcodeFormat.QR_CODE, width, height);
